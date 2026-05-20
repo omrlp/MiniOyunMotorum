cdfdbb37c365839d6a42d92ad549e3aadeb7265a
omrlp <sanaticinsanat2@gmail.com>
2026-05-20 16:08:07 +0300

    refactor: overhaul game architecture with dynamic menu management and Factory Method updates.
    `karakterler` ve `esyalar` listeleri global scope'a taşındı.
    `self.menuler` yapısı metot referansları yerine class instance'larını ve başlıkları tutan dinamik bir dictionary formatına dönüştürüldü.
    `game` sınıfına dynamic menu execution sağlayan ve seçilen modun `run` metodunu tetikleyen `menucalistir` fonksiyonu eklendi.
    `gamemodeEditor` ve `gamemodePVP` sınıflarına mod mantığını kapsülleyen `run` metotları eklendi.
    `characters` sınıfının `__init__` constructor yapısına eksik olan `name` parametresi eklendi ve `super()` ile üst sınıfa paslandı.
    `CharacterFactory.create_character` metodundaki kafa karıştırıcı `character` parametresi `name` olarak refactor edildi.
    Ana instance ismi `motor` yerine `oyun` olarak güncellendi ve default karakterler global listeye append edildi.
    Dosya sonundaki legacy procedural `while True` döngüsü ve kullanılmayan comment satırları tamamen clean up edildi.

--- oyunmotorumkod2.py
+++ oyunmotorumkod2.py
@@ -2,27 +2,46 @@
 import string
 from unicodedata import name
 
+karakterler = []
+esyalar = []
+
 class game:
     
     def __init__(self):
-        self.karakterler = []
-        self.esyalar = []
-        self.menuler = {1: self.gamemodePVP, 2: self.gamemodeEditor, 3: self.exitgame}
- 
-  
+        self.menuler = {1:{"isim": "Cikis", "class": None} , 2: {"isim": "Editor", "class": gamemodeEditor()}, 3: {"isim": "PVP", "class": gamemodePVP()}}
+
     def exitgame(self):
         print ("Oyundan cikiliyor...")
         exit()    
      
     def menusecimi(self):
-        print("Lütfen bir menü seçeneği belirleyin: (1 - PVP / 2 - Editor / 3 - Cikis)")
+        print("Lütfen bir menü seçeneği belirleyin: (1 - Cikis / 2 - Editor / 3 - PVP )")
         secim = input("Seciminizi giriniz: ")
         return int(secim)
         
-    
+    def menucalistir(self):
+        while True:
+            secim = self.menusecimi()
+            
+            if secim == 1:
+                self.exitgame()
+                
+            secilenmode = self.menuler.get(secim)
+            
+            if secilenmode and secilenmode["class"] :
+                secilenmode["class"].run()
+            else:
+                print("Gecersiz secim, lutfen tekrar deneyin.")
 class gamemodeEditor:
     def __init__(self):
         pass
+    
+    def run(self):
+        self.editorsecimi()
+        secim = input("Seciminizi giriniz: ")
+        if secim == "1":
+            self.karakterekle()
+            
     def editorsecimi(self):
         print ("yapmak istiginiz islemi seçebilirsiniz: (1 - yeni karakter ekleme / 2 - karakter silme / 3 - yeni eşya ekleme / 4 - eşya silme)")
         
@@ -39,16 +58,19 @@ class gamemodeEditor:
 class gamemodePVP:
     def __init__(self):
         pass
+    def run(self):
+        print ("PVP moduna gectiniz.")
     
 class gameobjects:
     def __init__(self, name):
         self.name = name
 
 class characters(gameobjects):
-    def __init__(self, zırh, hasar, can):
+    def __init__(self, name, zırh, hasar, can):
         super().__init__(name)
         self.zırh = zırh
         self.hasar = hasar
+        self.can = can
         
         
 class Esyalar(gameobjects):
@@ -57,8 +79,8 @@ class Esyalar(gameobjects):
      
 class CharacterFactory:
     @staticmethod
-    def create_character(character, zırh, hasar, can):
-        return characters(character, zırh, hasar, can)
+    def create_character(name ,zırh, hasar, can):
+        return characters(name, zırh, hasar, can)
      
 class EsyaFactory:
     @staticmethod
@@ -66,25 +88,16 @@ class EsyaFactory:
         return Esyalar(esya)  
      
      
-motor = game()     
-motor.karakterler.append(CharacterFactory.create_character("Sovalye", 3, 4, 30))
-motor.karakterler.append(CharacterFactory.create_character("Iblis", 1, 2, 45))
-motor.karakterler.append(CharacterFactory.create_character("Okcu", 0, 8, 20))
-motor.karakterler.append(CharacterFactory.create_character("Buyucu", 1, 6, 25))
+
+
+oyun = game()     
+karakterler.append(CharacterFactory.create_character("Sovalye", 3, 4, 30))
+karakterler.append(CharacterFactory.create_character("Iblis", 1, 2, 45))
+karakterler.append(CharacterFactory.create_character("Okcu", 0, 8, 20))
+karakterler.append(CharacterFactory.create_character("Buyucu", 1, 6, 25))
 
 print ("Oyuna Hosgeldiniz!")
 
-while True:
-    secim =game.menusecimi()
-      
-    if secim in game.menuler:
-        game.menuler[secim]()
-    else:
-        print("Gecersiz secim, lutfen tekrar deneyin.")    
-    
+oyun.menucalistir()
 
-#if (menu == 1) #pvp için yaptım
-	#print (" 1 numaralı oyuncu karakterini seçebilir:")
-  
-#else if (menu == 2) #editor modu içinyaptım
-	#print ("yapmak istiginiz islemi seçebilirsiniz: (1 - yeni karakter ekleme / 2 - karakter silme / 3 - yeni eşya ekleme / 4 - eşya silme)")
+    
